b476f10318d1023b86281d327ea01d2ef4b93c34
subham-kr-shaw <hisubham2002@gmail.com>
2026-05-04 20:50:22 +0530

    done

--- e-commerce-web/src/config/apiConfig.js
+++ e-commerce-web/src/config/apiConfig.js
@@ -1,24 +1,15 @@
-// import axios from "axios";
-
-// export const API_BASE_URL="https://shopsphere-backend-oe5a.onrender.com";
-
-// const jwt=localStorage.getItem("jwt");
-
-// export const api=axios.create({
-//     baseURL:API_BASE_URL,
-//     headers:{
-//         "authorization":`Bearer ${jwt}`,
-//         "content-type":"application/json"
-//     }
-// })
-
 import axios from "axios";
 
 export const API_BASE_URL="https://shopsphere-backend-oe5a.onrender.com";
 
+const jwt=localStorage.getItem("jwt");
+
 export const api=axios.create({
     baseURL:API_BASE_URL,
     headers:{
-        "content-type":"application/json"  // ✅ removed the hardcoded jwt line
+        "authorization":`Bearer ${jwt}`,
+        "content-type":"application/json"
     }
 })
+
+
